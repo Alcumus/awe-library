5259205f344bfe1a596a631b3aab93230c35f555
Andy Davies <andrew.davies@alcumusgroup.com>
2020-09-21 19:34:05 +0100

    patch / make develop branch

--- runtime/lib/simple-db.js
+++ runtime/lib/simple-db.js
@@ -5,7 +5,6 @@ import { handle, raiseAsync } from 'common/events'
 import { getItem, using } from 'common/using-local-storage-key'
 import { showNotification } from 'common/modal'
 import { initialize } from 'common/offline-data-service/behaviour-cache'
-import { isOnline } from 'common/utils'
 
 export const {
     resetStorage,
@@ -110,7 +109,7 @@ handle('hydrate.*', async (document) => {
 let retry = false
 
 const processQueue = async function processQueue() {
-    if (!isOnline()) return
+    if (!navigator.onLine) return
     if (window.processingQueue) {
         retry = true
         return
